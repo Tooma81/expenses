22a5fe9fb5fb272292ae115396170ee4aa636092
Tooma81 <toomas.kaarel@gmail.com>
2025-06-18 16:42:02 +0300

    removed date from expensesitem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,10 +3,6 @@ import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
 
-    const day = props.data.date.toLocaleString('en-US', {day: '2-digit'})
-    const month = props.data.date.toLocaleString('en-US', {month: 'long'})
-    const year = props.data.date.getFullYear()
-
     return (
         <div className='expense-item'>
             <ExpenseDate date={props.data.date}/>
