ea51f1ce35de206facdc90ac883f8d024378cedf
Tooma81 <toomas.kaarel@gmail.com>
2025-06-18 22:43:33 +0300

    Added ExpensesFilter component and classes

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,20 @@
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter'
 import Card from '../UI/Card';
 import './Expenses.css';
+import { useState } from 'react';
 
 const Expenses = (props) => {
+    const [filterYear, setFilterYear] = useState('2024')
+
+    const filterChange = (year) => {
+        setFilterYear(year)
+        console.log(`Year date in Expenses.js ${year}`)
+    }
 
     return(
         <Card className='expenses'>
+            <ExpensesFilter selectedYear={filterYear} onChangeFilter={filterChange} />
             <ExpenseItem data={props.expenses[0]}/>
             <ExpenseItem data={props.expenses[1]}/>
         </Card>
