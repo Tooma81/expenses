d6d6e1a2c67210a19f4cddc02ba24ce6ce953a31
Tooma81 <toomas.kaarel@gmail.com>
2025-06-18 17:15:31 +0300

    added expenses component and style

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,6 +1,7 @@
 import './ExpenseDate.css';
 
 const ExpenseDate = (props) => {
+    
     const day = props.date.toLocaleString('en-US', {day: '2-digit'})
     const month = props.date.toLocaleString('en-US', {month: 'long'})
     const year = props.date.getFullYear()
